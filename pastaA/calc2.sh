2960564b381cbc86c141ff43b0168945dfe805b2
thiago <thiagocondiolov@hotmail.com>
2025-02-09 11:19:48 -0300

    Comando `echo` adicionado para imprimir a mensagem "CALCULADORA SIMPLES".
    Comando `read` adicionado para capturar o primeiro número do usuário.
    
    Comando `read` adicionado para capturar a operação do usuário.
    
    Comando `read` adicionado para capturar o segundo número do usuário.

---
+++ pastaA/calc2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "CALCULADORA SIMPLES"
+# Entrada do usuário
+echo "Digite o primeiro número:"
+read num1
+
+echo "Digite a operação (+, -, *, /):"
+read operacao
+
+echo "Digite o segundo número:"
+read num2
